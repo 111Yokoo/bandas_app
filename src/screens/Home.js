6de80d873c923322d9da377b90baf7002196f06c
111Yokoo <yokoomatheus@gmail.com>
2024-04-11 10:18:16 -0300

    Finalização

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -2,19 +2,31 @@ import React, {useEffect, useState} from "react";
 import { View, Text, StyleSheet, StatusBar, FlatList } from "react-native";
 import MusicItem from "../components/MusicItem";
 import { useSafeAreaFrame } from "react-native-safe-area-context";
+import {Audio} from "expo-av";
 
 export default function Home({navigation}) {
   const [currentPlaying, setCurrentPlaying] = useState(null);
   const [musicData, setMusicData] = useState([]);
-  const item = {
-    id: '1',
-    title: "Highway to hell",
-    group: "AC/DC",
-    album_image: "https://m.media-amazon.com/images/I/711Hf7rJhFL._UF1000,1000_QL80_.jpg",
-    album: "Higway to Hell",
-    year: 1970,
-    genre: "Heavy Metal",
+  const [currentSound, setCurrentSound] = useState(null);
+
+  const togglePlayPause = async (item) => {
+    if(currentSound && currentPlaying == item.id){
+        await currentSound.pauseAsync();
+        setCurrentPlaying(null);
+        setCurrentSound(null);
+    }else{
+      if(currentSound){
+        await currentSound.unloadAsync();
+      }
+      const {sound} = await Audio.Sound.createAsync(
+        {uri: `http://10.0.2.2:3000/src/musics/${item.music_path}`},
+        {shouldPlay: true}
+      );
+      setCurrentSound(sound);
+      setCurrentPlaying(item.id);
+    }
   }
+  
   useEffect(() => {
     fetch("http://10.0.2.2:3000/musics")
     .then((response) => response.json())
@@ -25,7 +37,7 @@ export default function Home({navigation}) {
       <StatusBar barStyle="light-content" backgroundColor="#121212"/>
       <Text style={styles.title}>Minhas músicas</Text>
       <FlatList data={musicData} keyExtractor={(item) => item.id.toString()}
-       renderItem={({item}) => <MusicItem isPlaying={() => currentPlaying == item.id} music={item} navigation={navigation} onPlayPause={() => {}}/>}/> 
+       renderItem={({item}) => <MusicItem isPlaying={currentPlaying === item.id} music={item} navigation={navigation} onPlayPause={() => togglePlayPause(item)}/>}/> 
     </View>
   );
 }
